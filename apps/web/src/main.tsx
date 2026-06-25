a2278af4903dd140ad88f61adf6ce0d413994941
Jimmy-02 <tainat1202@gmail.com>
2026-06-25 16:57:57 +0700

    chore: tanstack query setup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,13 +4,17 @@ import { ClerkProvider } from "@clerk/react";
 
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
 );
