00933b72f236efd03a2a7bfdf08b2b9e9efa9901
Jimmy-02 <tainat1202@gmail.com>
2026-06-25 16:09:10 +0700

    feat: add create video inv

--- apps/api/src/routes/orderRouter.ts
+++ apps/api/src/routes/orderRouter.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import { getOrder, listOrders, createStreamChannel } from "../controllers/orderController";
+import { getOrder, listOrders, createStreamChannel, createVideoInvite } from "../controllers/orderController";
 
 const router = Router();
 
 router.get("/", listOrders);
 router.get("/:id", getOrder);
 router.post("/:id/stream-channel", createStreamChannel);
+router.post("/:id/video-invite", createVideoInvite);
 
 export default router;
