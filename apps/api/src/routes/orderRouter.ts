50226a5cc607b85422096076393b2d068ff3307e
Jimmy-02 <tainat1202@gmail.com>
2026-06-25 16:00:22 +0700

    feat: add stream channel

--- apps/api/src/routes/orderRouter.ts
+++ apps/api/src/routes/orderRouter.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { getOrder, listOrders } from "../controllers/orderController";
+import { getOrder, listOrders, createStreamChannel } from "../controllers/orderController";
 
 const router = Router();
 
 router.get("/", listOrders);
 router.get("/:id", getOrder);
+router.post("/:id/stream-channel", createStreamChannel);
 
 export default router;
