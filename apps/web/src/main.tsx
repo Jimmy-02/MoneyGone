b5c1ec1f490e74ef8b3f1c3113afa371db19d8d8
Jimmy-02 <tainat1202@gmail.com>
2026-06-25 17:46:11 +0700

    feat: add sentry error fallback

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,13 @@
+import App from "./App.tsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
+
+import { SentryErrorFallback } from "./components/SentryErrorFallback.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import App from "./App.tsx";
-import "./index.css";
-import * as Sentry from "@sentry/react";
+import { BrowserRouter } from "react-router";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -37,7 +39,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <Sentry.ErrorBoundary fallback={<SentryErrorFallback/>}>
+            <App />
+          </Sentry.ErrorBoundary>
+        </BrowserRouter>
       </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
