74a949daec87a4d8398bc1494f33f6505e4ddc6d
Jimmy-02 <tainat1202@gmail.com>
2026-06-27 21:26:55 +0700

    feat: add authenticated fetch api

--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -1,14 +1,20 @@
-import type { PropsWithChildren } from "react";
+import type { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-function Layout({ children }: PropsWithChildren) {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-svh flex-col bg-base-200 text-base-content">
       <Navbar />
-      <main className="mx-auto bg-red-600 w-full max-w-7xl flex-1 px-4 py-8 md:px-6 md:py-10">
+
+      <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-8 md:px-6 md:py-10">
         {children}
       </main>
+
       <Footer />
     </div>
   );
