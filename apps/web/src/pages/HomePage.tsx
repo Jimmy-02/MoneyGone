72c57664222ac24b33444779cb40f6cf13a075bd
Jimmy-02 <tainat1202@gmail.com>
2026-06-30 21:46:52 +0700

    feat: add trust strip

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { HomeHero } from "../components/HomeHero";
+import { TrustStrip } from "../components/TrustStrip";
 import { useHomeCatalog } from "../hooks/useHomeCatalog";
 
 const HomePage = () => {
@@ -10,6 +11,8 @@ const HomePage = () => {
   return (
     <div className="space-y-12">
       <HomeHero categories={categories} loadingCategories={loadingCategories} />
+
+      <TrustStrip/>
     </div>
   );
 }
